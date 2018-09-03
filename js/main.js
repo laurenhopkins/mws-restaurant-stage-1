77558ffb29e5c7781db379a42f48e503cf32dbfe
Lauren Hopkins <laurenhop7@gmail.com>
2018-09-03 18:42:54 -0400

    Adds Service Worker

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+// Make sure service worker is supported
+if('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('../sw.js')
+      .then(reg => console.log('Service Worker: Registered'))
+      .catch(err => console.log(`Service Worker: Error: ${err}`))
+  });
+}
+
 let restaurants,
   neighborhoods,
   cuisines
